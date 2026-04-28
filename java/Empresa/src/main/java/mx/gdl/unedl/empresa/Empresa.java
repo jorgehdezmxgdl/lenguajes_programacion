fcb759647c31de30d5e860414c6d0c54e3aeaed8
jorgehdezmxgdl <67309059+jorgehdezmxgdl@users.noreply.github.com>
2026-04-28 09:23:42 -0600

    Editor de textos

---
+++ java/Empresa/src/main/java/mx/gdl/unedl/empresa/Empresa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.gdl.unedl.empresa;
+
+/**
+ *
+ * @author jorgefausto
+ */
+public class Empresa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
